3bc3c19755223fbb7c97b09169425577120d0bbd
Jack Yammine <jackyammine01@gmail.com>
2020-08-08 18:57:35 +0300

    Added html, css and js files for browse

---
+++ src/browse.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './browse.css';
+import './browse.html';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
